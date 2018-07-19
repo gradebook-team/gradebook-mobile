584105303f5d1ae99add66b10561f0cca96507a2
Nathan Yan <s-yann@bsd405.org>
2018-07-18 18:57:06 -0700

    Moved files out of 'gradebook' folder

---
+++ Api/Api.js
@@ -0,0 +1,43 @@
+import { fetch } from 'fetch';
+
+const BASE_URL = 'http://192.168.1.9:5000'
+
+exports.login = async function (username, password) {
+    try {
+        console.log(JSON.stringify({
+            "username" : username,
+            "password" : password
+        }))
+        let res = await fetch(BASE_URL + "/login", {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify({
+                "username": username,
+                "password": password
+            }),
+            mode: "no-cors"
+        })
+
+        let json = await res.json();
+        res.json = json;
+
+        return res;
+    }catch(err){
+        throw err
+    }
+}
+
+exports.fetchGrades = async function () {
+    try {
+        let res = await fetch(BASE_URL + "/grades", {
+            method: "GET",
+            headers: {
+                Accept: "application/json"
+            },
+            // credentials: "include": will include with integration
+        })
+    }
+}
