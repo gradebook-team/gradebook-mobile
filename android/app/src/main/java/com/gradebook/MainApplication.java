4195327414450c7c70b4687da29bdf8ad4fc352f
Nathan Yan <s-yann@bsd405.org>
2018-07-19 03:00:55 -0700

    Linked react-native-linear-gradient

--- android/app/src/main/java/com/gradebook/MainApplication.java
+++ android/app/src/main/java/com/gradebook/MainApplication.java
@@ -5,6 +5,8 @@ import com.horcrux.svg.SvgPackage;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,7 +26,9 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(), new SvgPackage()
+          new MainReactPackage(),
+            new SvgPackage(),
+            new LinearGradientPackage()
       );
     }
 
